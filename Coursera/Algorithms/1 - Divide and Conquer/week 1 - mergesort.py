8dde3e1cf3b1ea80166c33b1d3eea199c47f7222
Taran <taranraghuram@Taran-Raghurams-MacBook-Pro.local>
2018-03-18 15:18:22 -0400

    made some updates for quiz

---
+++ Coursera/Algorithms/1 - Divide and Conquer/week 1 - mergesort.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+""" Mergesort - consists of a recursive steps that merge sorted sub-arrays of the input array.
+
+Two versions of merge sort - first does merges by cutting array into half in each recursive call
+
+Second version asks for the "split" amount
+
+Both should take same amount of time since having fewer recursive calls means more merges
+
+quiz answers at the top
+
+"""
+
+
+"""
+Quiz for week 1
+
+1) nlog(n) - mergesort results in nlog(n) time for any subdivision length (will
+always cancel out with the number of recursive calls)
+
+2) True! See notebook
+
+3) 
+
+
+
+
+"""
+
+def mergesort_1(n):
+    
