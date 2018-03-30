48e8e3d4f2f750c3f9145fd7ce42aa8451610efb
Taran <taran.raghuram@gmail.com>
2018-03-29 22:39:43 -0400

    inversions works!
    need to read array file into

--- Coursera/Algorithms/1 - Divide and Conquer/week 2 - master method.py
+++ Coursera/Algorithms/1 - Divide and Conquer/week 2 - master method.py
@@ -6,13 +6,48 @@ Created on Wed Mar 28 21:32:26 2018
 @author: taranraghuram
 """
 
+"""
+with open("integer_array.txt") as f:
+    lines = f.read().splitlines()
+"""
 
-def count_inversions(n):
+
+def inversion_sort(n):
     '''
-    returns a sorted list and the number of inversions required
+    returns a sorted list and the number of inversions made
     
     '''    
-
+    
+    result = []
+    
+    if len(n) == 1:
+        return [n,0]
+    
+    #if not base case, recursively sort arrays and keep track of inversions
+    
+    n1 = inversion_sort(n[:len(n)//2])
+    n2 = inversion_sort(n[len(n)//2:])
+    
+    print(n1)
+    
+    i = 0
+    j = 0
+    inversions = 0
+    
+    while i < len(n1[0]) and j < len(n2[0]):
+        if n1[0][i] > n2[0][j]:
+            result.append(n2[0][j])
+            inversions += len(n1[0])-i
+            j += 1
+        else:
+            result.append(n1[0][i])
+            i += 1
+    result += n1[0][i:]
+    result += n2[0][j:]
+    inversions = inversions + n1[1] + n2[1]
+    
+    return [result, inversions]
+    
 
 def mergesort(n):
     '''
