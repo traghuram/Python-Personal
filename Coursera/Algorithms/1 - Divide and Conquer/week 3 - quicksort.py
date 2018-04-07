cf181435db8223b508b885d821b060aa0dd997ef
traghuram <taran.raghuram@gmail.com>
2018-04-07 18:56:50 -0400

    Quicksort fixed

--- Coursera/Algorithms/1 - Divide and Conquer/week 3 - quicksort.py
+++ Coursera/Algorithms/1 - Divide and Conquer/week 3 - quicksort.py
@@ -8,9 +8,15 @@ Created on Mon Apr  2 18:41:01 2018
 #quicksort - l is starting position, r is end position. For first elem pivot
 
 def quicksort_first(A, l=0, r=0):
-        
+    '''Sorts an array recursively
+        Returns a sorted list
+        Assumes some elements, no repeated elements (for now)
+    '''
+    
+    print(A, l, r)
+    
     if r==l:
-        print("At the end!")
+        return A
     
     else:
         p = A[l]
@@ -20,13 +26,15 @@ def quicksort_first(A, l=0, r=0):
             if A[j] < p:
                 A[i], A[j] = A[j], A[i]
                 i += 1
-                print(A)
         
         A[l], A[i-1] = A[i-1], A[l]
-        print(A)
-        
-        quicksort_first(A,0,i-2)
-        quicksort_first(A,i,len(A))
-        
-        print(A)
+                
+        #if there are 1 or fewer elements to the left of the pivot, don't do left quicksort
+        #elif there are no elements to the right of the pivot, do right quicksort
+        #else do both
+
+        quicksort_first(A,0,i-1)
+        quicksort_first(A,i,r)
+                
+        return A
     
