4f8321a8282b97b4c1369daf1d838119abfa638e
traghuram <taran.raghuram@gmail.com>
2018-05-15 22:40:35 -0400

    Added a binary search
    for strings

--- EdX/6001/exercises.py
+++ EdX/6001/exercises.py
@@ -103,7 +103,21 @@ while True:
 print("Game over. The secret number was: " + str(guess))
 
 
-
+def isIn(char, aStr):
+    '''
+    char: a single character
+    aStr: an alphabetized string
+    
+    returns: True if char is in aStr; False otherwise
+    '''
+    if len(aStr) == 0 or len(aStr) == 1 and char != aStr:
+        return False
+    elif char == aStr[len(aStr)//2]:
+        return True    
+    elif char < aStr[len(aStr)//2]:
+        return isIn(char, aStr[0:len(aStr)//2])
+    else:
+        return isIn(char, aStr[len(aStr)//2:len(aStr)])
 
 
 def gcdIter(a, b):
