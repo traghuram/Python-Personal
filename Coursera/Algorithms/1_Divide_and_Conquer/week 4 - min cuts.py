9a19f6659758e0fe0109679d2661c7403aceb095
traghuram <taran.raghuram@gmail.com>
2021-08-29 15:55:28 -0400

    Min cuts
    Updated min cuts

---
+++ Coursera/Algorithms/1_Divide_and_Conquer/week 4 - min cuts.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 11 08:30:21 2021
+
+@author: Taran
+"""
+
+'''
+Quiz answers
+
+1. n - 1
+
+2. 
+
+3. 2*alpha - 1
+
+4. log(n)/-log(alpha)
+
+5. 
+
+
+Pset:
+    
+
+'''
+
+## Create one function to run the random contraction algorithm once
+
+
+
+## Create one function to run algorithm many times and record min cut
